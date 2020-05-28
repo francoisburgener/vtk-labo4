ed58309512daab1975bbba65b6b8b84b0ccba6f9
tiagoquin <tiago.quin@gmail.com>
2020-05-28 12:16:45 +0200

    display knee with correct camera and neat background

--- src/knee.py
+++ src/knee.py
@@ -1,6 +1,8 @@
 import vtk
 import time
 
+colors = vtk.vtkNamedColors()
+
 
 def read_slc_file(filename):
     print("Reading SLC file")
@@ -14,37 +16,55 @@ def read_slc_file(filename):
 
 def main():
     start = time.perf_counter()
-    slc_data = read_slc_file('vw_knee.slc')
+    reader_slc_data = read_slc_file('vw_knee.slc')
     end = time.perf_counter()
 
     print(f"Reading SLC file in {end - start:0.4f} seconds")
 
     print("Create mapper")
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(slc_data.GetOutputPort())
+    mapper.SetInputConnection(reader_slc_data.GetOutputPort())
+
+    contourFilter = vtk.vtkContourFilter()
+    contourFilter.SetInputConnection(reader_slc_data.GetOutputPort())
+    contourFilter.SetValue(0, 72.0)
+
+    outliner = vtk.vtkOutlineFilter()
+    outliner.SetInputConnection(reader_slc_data.GetOutputPort())
+    outliner.Update()
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(contourFilter.GetOutputPort())
+    mapper.SetScalarVisibility(0)
 
     print("Create actor")
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
-    actor.SetScale(0.1, 0.1, 0.1)
-    actor.GetProperty().SetColor(0.5, 0.5, 0.5)
-    actor.GetProperty().SetRepresentationToSurface()
 
     print("Create a rendering window and renderer")
     ren = vtk.vtkRenderer()
+    ren_win = vtk.vtkRenderWindow()
     ren.AddActor(actor)
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    ren.SetBackground(1, 1, 1)
+    ren_win.AddRenderer(ren)
+    ren.SetBackground(colors.GetColor3d("pink_light"))
 
     print("Create a renderwindowinteractor")
     iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
+    iren.SetRenderWindow(ren_win)
+
+    ren.GetActiveCamera().SetPosition(-382.606608, -3.308563, 223.475751)
+    ren.GetActiveCamera().SetFocalPoint(77.311562, 72.821162, 100.000000)
+    ren.GetActiveCamera().SetViewUp(0.235483, 0.137775, 0.962063)
+    ren.GetActiveCamera().SetDistance(482.25171)
+    ren.GetActiveCamera().SetClippingRange(27.933848, 677.669341)
+    ren.GetActiveCamera().Roll(180.)
+    ren.GetActiveCamera().Azimuth(180.)
+    ren_win.Render()
 
     print("Render")
     # Enable user interface interactor
     iren.Initialize()
-    renWin.Render()
+    ren_win.Render()
     iren.Start()
 
 
